65d6551c3a08aa5ddf20fed74759a7c440063c94
Shams Ali <ali.s90@outlook.com>
2016-05-23 22:40:39 -0700

    added bmi class

--- server/stats/statModel.js
+++ server/stats/statModel.js
@@ -6,17 +6,35 @@ var StatSchema = new mongoose.Schema({
   weight: Number,
   height: Number,
   bmi: Number,
-  class: 'text'
+  bmiClass: String
 });
 
 var calculateBMI = function (weight, height) {
   var bmi = weight / (height * height);
-  return Math.round(bmi * 703);
+  return bmi * 703;
+};
+
+var calculateClass = function(bmi) {
+  if (bmi <= 18.5) {
+    return 'UnderWeight';
+  } else if (bmi <= 24.99) {
+    return 'Normal Weight';
+  } else if (bmi <= 29.99) {
+    return 'Overweight';
+  } else if (bmi <= 34.99) {
+    return 'Obesity (class 1)';
+  } else if (bmi <= 39.99) {
+    return 'Obesity (class 2)';
+  } else if (bmi >= 40) {
+    return 'Morbid Obesity';
+  }
 };
 
 StatSchema.pre('save', function (next) {
   var bmi = calculateBMI(this.weight, this.height);
-  this.bmi = bmi;
+  var bmiClass = calculateClass(bmi);
+  this.bmi = Math.round(bmi);
+  this.bmiClass = bmiClass;
   next();
 });
 
